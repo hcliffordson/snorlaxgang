7aa0ac8f4e3304f45592e1046da81dc0a3b56aec
oggewallgren <oskarwallgren@icloud.com>
2019-04-08 15:05:16 +0200

    made product card for use in grid

---
+++ development/frontend/src/components/ProductCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">Product Title</p>
+          <p class="subtitle is-6">$$$$$$$</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
