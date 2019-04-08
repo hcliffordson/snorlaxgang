6a572c2540889bdf499fa5beccfe1ebe8f0d9ee5
oggewallgren <oskarwallgren@icloud.com>
2019-04-08 15:31:15 +0200

    made product cards use props, title, price and img

--- development/frontend/src/components/ProductCard.vue
+++ development/frontend/src/components/ProductCard.vue
@@ -2,16 +2,31 @@
   <div class="card">
     <div class="card-image">
       <figure class="image is-4by3">
-        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+        <img :src="imgURL" alt="Placeholder image">
       </figure>
     </div>
     <div class="card-content">
       <div class="media">
         <div class="media-content">
-          <p class="title is-4">Product Title</p>
-          <p class="subtitle is-6">$$$$$$$</p>
+          <p class="title is-4">{{ title }}</p>
+          <p class="subtitle is-6">{{ price }}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ProductCard',
+  props: {
+    title: String,
+    price: Number,
+    imgURL: String
+  }
+});
+</script>
+
+
