8ce3cc88184f02643d4210773fc7d42a3a345566
vltmn <melkerveltman@gmail.com>
2019-04-08 15:04:25 +0200

    added queries and resolvers for getting all listings and a specific listing

--- development/backend/src/resolvers/query.ts
+++ development/backend/src/resolvers/query.ts
@@ -2,7 +2,17 @@ import { Query } from './utils';
 import { IResolverObject } from 'graphql-tools';
 
 export const QueryMap: Query & IResolverObject = {
-    test: async (_parent, args, ctx, info) => {
-        return 'Hello world!';
+    getAllListings: async (_parent, _args, ctx, info) => {
+        return ctx.binding.query.listings({}, info);
+    },
+
+    getListing: async (_parent, args, ctx, info) => {
+        const ID = args.id;
+        return ctx.binding.query.listing(
+            {
+                where: {
+                    id: ID
+                }
+            }, info);
     }
 };
