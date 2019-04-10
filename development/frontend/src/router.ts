7e1d8a58ed1ad6d4c4b85a5812b08f7fd57e9a34
hcliffordson <cliffords.contact@gmail.com>
2019-04-10 13:29:39 +0200

    added route for one listing

--- development/frontend/src/router.ts
+++ development/frontend/src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Listing from './views/Listing.vue';
 
 Vue.use(Router);
 
@@ -21,5 +22,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/listing/:id',
+      name: 'listingDetail',
+      component: Listing
+    }
   ],
 });
---
+++ development/frontend/src/views/Listing.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p> {{ $route.params.id }} </p>
+  </div>
+</template>
