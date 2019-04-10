f97aad4f4c367d06f480c5eec9bdf031d43b70e2
vltmn <melkerveltman@gmail.com>
2019-04-10 15:22:52 +0200

    render product grid on homepage

--- development/frontend/src/views/Home.vue
+++ development/frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <product-grid-data />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ProductGridData from '@/components/ProductGridData.vue';
 
 export default Vue.extend({
   name: 'home',
   components: {
-    HelloWorld,
+    ProductGridData
   },
 });
 </script>
