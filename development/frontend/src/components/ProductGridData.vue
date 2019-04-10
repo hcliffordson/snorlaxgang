0c309e1f0292533b3749e35dffe16def627857c7
hcliffordson <cliffords.contact@gmail.com>
2019-04-10 13:29:52 +0200

    added productgriddata component

---
+++ development/frontend/src/components/ProductGridData.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{ getAllListings }}</div>
+</template>
+
+<script lang="ts">
+import gql from 'graphql-tag';
+
+import Vue from 'vue';
+export default Vue.extend({
+  apollo: {
+    getAllListings: gql`
+      query {
+        getAllListings {
+          price
+          title
+          description
+        }
+      }
+    `
+  }
+});
+</script>
