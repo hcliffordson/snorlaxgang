a85448d3664154460ffc7b99f5eeff33fb07c04e
Oscar <oscar.wiss@gmail.com>
2019-04-15 14:46:48 +0200

    Added navbar

--- development/frontend/src/App.vue
+++ development/frontend/src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
+    <NavBar/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ development/frontend/src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"
+        style="margin-left: 0"> <!-- not great but no left-adjusted burgers in bulma -->
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+        <a class="navbar-item" href="home">
+          <img src="../assets/otrade512.png" width="28" height="28">
+        </a>
+        <div class="navbar-item" style="margin-left: 180px">
+            <a class="button is-primary" href="publish"> Add
+              <strong></strong>
+            </a>
+        </div>
+      </div>
+  </nav>
+</template>
