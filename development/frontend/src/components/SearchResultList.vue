b183cbf2ab7c0e85945cfb7f013358e5ce18e1ab
Oscar <oscar.wiss@gmail.com>
2019-05-13 16:24:26 +0200

    added a list of search result items

---
+++ development/frontend/src/components/SearchResultList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="result in searchResults" v-bind:key="result.id">
+      <router-link :to="'/listing/' + result.id">
+        <SearchResultItem :title="result.title" :price="result.price" :imgURL="result.imgURL" >
+        </SearchResultItem>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import SearchResultItem from '@/components/SearchResultItem.vue'; // @ is an alias to /src
+
+export default Vue.extend({
+  name: 'SearchResultList',
+  components: {
+    SearchResultItem
+  },
+  props: {
+    searchResults: Array,
+  }
+});
+</script>
