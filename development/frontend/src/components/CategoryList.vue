a50141c8dbcda22db829bdced2af3f97efefa893
hcliffordson <cliffords.contact@gmail.com>
2019-05-13 14:49:53 +0200

    created components for list of categories

---
+++ development/frontend/src/components/CategoryList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <div v-for="category in categories" v-bind:key="category.id">
+    <h1>
+      {{ category.label }}
+      {{ category.id }}
+    </h1>
+  </div>
+  </div>
+
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'CategoryList',
+  props: {
+    categories: Array,
+  }
+});
+</script>
---
+++ development/frontend/src/components/CategoryListData.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <category-list v-bind:categories="categories" />
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+import Vue from 'vue';
+import CategoryList from '@/components/CategoryList.vue';
+import {GET_ALL_CATEGORIES_QUERY} from '@/services/backend';
+
+const apolloCategories = {
+  query: gql`${GET_ALL_CATEGORIES_QUERY}`,
+  update(data) {
+    console.log(data.getAllCategories);
+    return data.getAllCategories;
+  }
+};
+export default Vue.extend ({
+  components: {
+    CategoryList
+  },
+
+  apollo: {
+    categories: apolloCategories
+  }
+});
+</script>
