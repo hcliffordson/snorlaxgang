3caa761a19432ecdaa9eebca6a877f9077c966d8
hcliffordson <cliffords.contact@gmail.com>
2019-05-20 11:30:41 +0200

    added update mutation for listing in backend

--- development/backend/src/resolvers/mutation.ts
+++ development/backend/src/resolvers/mutation.ts
@@ -1,5 +1,7 @@
 import { Mutation } from './utils';
 import { IResolverObject } from 'graphql-tools';
+import { argumentsObjectFromField } from 'apollo-utilities';
+import { UserInputError } from 'apollo-server-core';
 
 export const MutationMap: Mutation & IResolverObject = {
     createListing: async (_parent, args, ctx, info) => {
@@ -27,5 +29,40 @@ export const MutationMap: Mutation & IResolverObject = {
                 }
             }
         }, info);
+    },
+
+    updateListing: async (_parent, args, ctx, info) => {
+        const { id, price, title, description, imgURL, categoryId } = args;
+        const userId = ctx.user.id;
+        const listingValid = await ctx.db.$exists.listing({
+            id,
+            createdBy: {
+                id: userId
+            }
+        });
+
+        if (!listingValid) {
+            throw new UserInputError('No valid listing supplied');
+        }
+        const mutation = await ctx.binding.mutation.updateListing({
+            where: {
+                id
+            },
+            data: {
+                price,
+                description,
+                title,
+                imgURL,
+                category: {
+                    connect: {
+                        id: categoryId
+                    }
+                }
+            },
+        }, info);
+        if (!mutation) {
+            throw new UserInputError('Could not be updated');
+        }
+        return mutation;
     }
 };
